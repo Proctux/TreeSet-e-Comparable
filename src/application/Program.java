f7a72fda4c9c5d669fa441b3da825728fefc8246
Luiz Fernando de Morais <luiz.ado@hotmail.com>
2019-03-30 15:59:07 -0300

    Project done

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,27 @@
 package application;
 
+import java.util.Locale;
+import java.util.Set;
+import java.util.TreeSet;
+
+import model.entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		
+		Set<Product> set = new TreeSet<>();
+		
+		set.add(new Product("Notebook", 1200.00));
+		set.add(new Product("TV", 900.00));
+		set.add(new Product("Tablet", 560.00));
+		
+		for(Product p : set) {
+			System.out.println(p);
+		}
+		
+		
 	}
 
 }
